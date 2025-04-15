453dd9f3be19bda7ca2961c7fde24c6a90fde3b0
윤범진 <youn7054@gmail.com>
2025-04-15 01:24:47 +0000

    [UPDATE] commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
             <SidebarMenuItem>
               <Icons.list className="mr-2 h-4 w-4" />
               <a href="#" onClick={() => router.push('/expenses')}>
-                Expenses
+                Expenses!!!!!!
               </a>
             </SidebarMenuItem>
             <SidebarMenuItem>
